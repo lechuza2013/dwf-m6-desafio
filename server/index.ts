a0abcb6a9a0e5082fdfcf3c2d507efd4c61edd1d
axel <julian_s_99@live.com>
2023-03-09 16:27:47 -0300

    probando api

--- server/index.ts
+++ server/index.ts
@@ -18,12 +18,15 @@ const userCollection = firestore.collection("users");
 const roomCollection = firestore.collection("rooms");
 
 app.get("/rooms", async (req, res)=>{
-   roomCollection.get().then((roomSnap)=>{
-      const roomCollectionSnap = roomSnap;
-      console.log("RoomSnap: ", roomCollectionSnap);
-      res.json(roomCollectionSnap);
-   });
+   res.json({"noob": "vos"});
+   console.log("noob");
+   // roomCollection.get().then((roomSnap)=>{
+   //    const roomCollectionSnap = roomSnap;
+   //    console.log("RoomSnap: ", roomCollectionSnap);
+   //    res.json(roomCollectionSnap);
+   // });
    // PROBAR SI LA API EN EL DEPLOY 
+   // PROBAR CON POSTMAN
 });
 
 app.listen(PORT, ()=>{
